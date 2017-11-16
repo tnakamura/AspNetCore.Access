1cf38df1df7632b8f03934e25e19165fb4d519dc
tnakamura <griefworker@gmail.com>
2017-11-16 11:15:49 +0900

    Write xml comment.

--- src/AspNetCore.Access/AccessOptions.cs
+++ src/AspNetCore.Access/AccessOptions.cs
@@ -3,13 +3,25 @@ using System.Linq;
 
 namespace AspNetCore.Access
 {
+    /// <summary>
+    /// Provides programmatic configuration for the Access middleware.
+    /// </summary>
     public class AccessOptions
     {
+        /// <summary>
+        /// Create a new instance of <see cref="AccessOptions"/>
+        /// </summary>
+        /// <param name="mappings">
+        /// The mappings configured in the <see cref="IAccessMappingBuilder"/>.
+        /// </param>
         public AccessOptions(ILookup<PathString, string> mappings)
         {
             Mappings = mappings;
         }
 
+        /// <summary>
+        /// Gets the mappings configured in the <see cref="IAccessMappingBuilder"/>.
+        /// </summary>
         public ILookup<PathString, string> Mappings { get; }
     }
 }
--- src/AspNetCore.Access/IAccessMappingBuilder.cs
+++ src/AspNetCore.Access/IAccessMappingBuilder.cs
@@ -7,12 +7,21 @@ namespace AspNetCore.Access
     /// </summary>
     public interface IAccessMappingBuilder
     {
+        /// <summary>
+        /// Adds a mapping to the <see cref="IAccessMappingBuilder"/>.
+        /// </summary>
+        /// <param name="path">The URL</param>
+        /// <param name="ipAddress">Accept IP address</param>
+        /// <param name="optionalIpAddresses">Accept IP addresses</param>
+        /// <returns>
+        /// A reference to this instance after the operation has completed.
+        /// </returns>
         IAccessMappingBuilder MapPath(PathString path, string ipAddress, params string[] optionalIpAddresses);
 
         /// <summary>
         /// Builds an <see cref="AccessOptions"/>
         /// </summary>
-        /// <returns></returns>
+        /// <returns>An <see cref="AccessOptions"/> instance.</returns>
         AccessOptions Build();
     }
 }
