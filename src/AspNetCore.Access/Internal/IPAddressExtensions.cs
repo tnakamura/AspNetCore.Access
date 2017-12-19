67589363e6dab94feac4ad592235d8a2fa2b80c5
tnakamura <griefworker@gmail.com>
2017-12-19 17:35:48 +0900

    Add IPAddressExtensions

---
+++ src/AspNetCore.Access/Internal/IPAddressExtensions.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AspNetCore.Access.Internal
+{
+    internal static class IPAddressExtensions
+    {
+        public static IPAddress GetBroadcastAddress(this IPAddress address, IPAddress subnetMask)
+        {
+            var ipAdressBytes = address.GetAddressBytes();
+            var subnetMaskBytes = subnetMask.GetAddressBytes();
+
+            if (ipAdressBytes.Length != subnetMaskBytes.Length)
+            {
+                throw new ArgumentException(
+                    "Lengths of IP address and subnet mask do not match.",
+                    nameof(subnetMask));
+            }
+
+            var broadcastAddress = new byte[ipAdressBytes.Length];
+            for (int i = 0; i < broadcastAddress.Length; i++)
+            {
+                broadcastAddress[i] = (byte)(ipAdressBytes[i] | (subnetMaskBytes[i] ^ 255));
+            }
+            return new IPAddress(broadcastAddress);
+        }
+
+        public static IPAddress GetNetworkAddress(this IPAddress address, IPAddress subnetMask)
+        {
+            var ipAdressBytes = address.GetAddressBytes();
+            var subnetMaskBytes = subnetMask.GetAddressBytes();
+
+            if (ipAdressBytes.Length != subnetMaskBytes.Length)
+            {
+                throw new ArgumentException(
+                    "Lengths of IP address and subnet mask do not match.",
+                    nameof(subnetMask));
+            }
+
+            var broadcastAddress = new byte[ipAdressBytes.Length];
+            for (int i = 0; i < broadcastAddress.Length; i++)
+            {
+                broadcastAddress[i] = (byte)(ipAdressBytes[i] & (subnetMaskBytes[i]));
+            }
+            return new IPAddress(broadcastAddress);
+        }
+
+        public static bool IsInSameSubnet(this IPAddress address2, IPAddress address, IPAddress subnetMask)
+        {
+            var network1 = address.GetNetworkAddress(subnetMask);
+            var network2 = address2.GetNetworkAddress(subnetMask);
+
+            return network1.Equals(network2);
+        }
+    }
+}
